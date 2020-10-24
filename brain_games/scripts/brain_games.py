2bc7d0ce8f37f6f617f848142f2266f00bb453dd
Алексей Малышев <alexey1978malyshev@gmail.com>
2020-10-25 00:10:01 +0300

    addedimport

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,12 +1,13 @@
 #!/usr/bin/env python3
-
+import sys  
+sys.path.insert(0, '/brain_games.cli/')  
+ 
 from cli import welcome_user
 
 def main():
     """Make a user intreface."""
     print('Welcome to the Brain Games!')
-    welcome_user()
-
+    
 if __name__ == '__main__':
     main()
-
+welcome_user()
