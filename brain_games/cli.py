b87c556e50b450d74d3554b3f7befaa76b5232d1
Alexey <alexey1978malyshev@gmail.com>
2020-10-26 17:12:35 +0300

    Update cli.py

--- brain_games/cli.py
+++ brain_games/cli.py
@@ -1,16 +1,5 @@
-<<<<<<< HEAD
-#!/usr/bin/env python3
-=======
-#!/usr/bin/env python
->>>>>>> bb231eb4fe889a13e94ab9f6d0f7903bab9d0a72
-
 import prompt
 
 def welcome_user():
 	name = prompt.string('May I have your name? ')
 	print('Hello, {0}!'.format(name))
-<<<<<<< HEAD
-        return name
-=======
-print(welcome_user())
->>>>>>> bb231eb4fe889a13e94ab9f6d0f7903bab9d0a72
