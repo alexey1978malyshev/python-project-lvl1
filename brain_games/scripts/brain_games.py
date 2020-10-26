27c7581eb8b1cff618c90f42e272aeb6b598695b
Alexey <alexey1978malyshev@gmail.com>
2020-10-26 17:13:43 +0300

    Update brain_games.py

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #!/usr/bin/env python3
 import sys  
 sys.path.insert(0, '/brain_games.cli/')  
@@ -8,18 +7,7 @@ from cli import welcome_user
 def main():
     """Make a user intreface."""
     print('Welcome to the Brain Games!')
-=======
-#!/usr/bin/env python
 
-from /home/alexey/python-project-lvl1 import cli
-def greet(who):
-    print('{}'.format(who))
-
-def main():
-    greet('Welcome to the Brain Games!')
-    print(cli.welcome_user)
->>>>>>> bb231eb4fe889a13e94ab9f6d0f7903bab9d0a72
-    
 if __name__ == '__main__':
     main()
 welcome_user()
